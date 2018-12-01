14632f7d547fe7a9cc29b2d1204035f0ed276ded
Tobias Göttfert <tobias.goettfert@posteo.de>
2018-12-01 11:39:33 +0100

    refactor tests

--- y2015/test/test_day01.py
+++ y2015/test/test_day01.py
@@ -1,18 +1,26 @@
-from y2015 import day01
+from y2015.day01 import *
 
 
 def test_part1():
-    assert day01.part1("()()") == 0
-    assert day01.part1("(())") == 0
-    assert day01.part1("(((") == 3
-    assert day01.part1("(()(()(") == 3
-    assert day01.part1("))(((((") == 3
-    assert day01.part1("())") == -1
-    assert day01.part1("))(") == -1
-    assert day01.part1(")))") == -3
-    assert day01.part1(")())())") == -3
+    i = (
+        ("()()", 0),
+        ("(())", 0),
+        ("(((", 3),
+        ("(()(()(", 3),
+        ("))(((((", 3),
+        ("())", -1),
+        ("))(", -1),
+        (")))", -3),
+        (")())())", -3)
+    )
+    for tc in i:
+        assert part1(tc[0]) == tc[1]
 
 
 def test_part2():
-    assert day01.part2(")") == 1
-    assert day01.part2("()())") == 5
+    i = (
+        (")", 1),
+        ("()())", 5)
+    )
+    for tc in i:
+        assert part2(tc[0]) == tc[1]
--- y2018/test/test_day01.py
+++ y2018/test/test_day01.py
@@ -1,4 +1,4 @@
-from y2018 import day01
+from y2018.day01 import *
 
 
 def to_input_format(i):
@@ -6,31 +6,23 @@ def to_input_format(i):
 
 
 def test_part1():
-    i = to_input_format("+1, -2, +3, +1")
-    assert day01.part1(i) == 3
-
-    i = to_input_format("+1, +1, +1")
-    assert day01.part1(i) == 3
-
-    i = to_input_format("+1, +1, -2")
-    assert day01.part1(i) == 0
-
-    i = to_input_format("-1, -2, -3")
-    assert day01.part1(i) == -6
+    i = (
+        ("+1, -2, +3, +1", 3),
+        ("+1, +1, +1", 3),
+        ("+1, +1, -2", 0),
+        ("-1, -2, -3", -6)
+    )
+    for tc in i:
+        assert part1(to_input_format(tc[0])) == tc[1]
 
 
 def test_part2():
-    i = to_input_format("+1, -2, +3, +1")
-    assert day01.part2(i) == 2
-
-    i = to_input_format("+1, -1")
-    assert day01.part2(i) == 0
-
-    i = to_input_format("+3, +3, +4, -2, -4")
-    assert day01.part2(i) == 10
-
-    i = to_input_format("-6, +3, +8, +5, -6")
-    assert day01.part2(i) == 5
-
-    i = to_input_format("+7, +7, -2, -7, -4")
-    assert day01.part2(i) == 14
+    i = (
+        ("+1, -2, +3, +1", 2),
+        ("+1, -1", 0),
+        ("+3, +3, +4, -2, -4", 10),
+        ("-6, +3, +8, +5, -6", 5),
+        ("+7, +7, -2, -7, -4", 14)
+    )
+    for tc in i:
+        assert part2(to_input_format(tc[0])) == tc[1]
