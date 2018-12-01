09aabd7b87af51d8a75b34494ae7aa867b2a205c
Tobias Göttfert <tobias.goettfert@posteo.de>
2018-12-01 11:39:50 +0100

    prepare for day2 of 2018

---
+++ y2018/day02.py
@@ -0,0 +1,15 @@
+def get_input():
+    return open("input_day02.txt").read()
+
+
+def part1(puzzle):
+    pass
+
+
+def part2(puzzle):
+    pass
+
+
+if __name__ == "__main__":
+    print("Part 1:", part1(get_input()))
+    print("Part 2:", part2(get_input()))
---
+++ y2018/test/test_day02.py
@@ -0,0 +1,14 @@
+from y2018.day02 import *
+
+
+
+def test_part1():
+    pass
+    #i = "bla"
+    #assert part1(i) == 3
+
+
+
+def test_part2():
+    pass
+    #assert part2("asd") == "nix"
