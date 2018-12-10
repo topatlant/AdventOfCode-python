f84539af44846484339459c73418ea510f1ab1de
Tobias Göttfert <tobias.goettfert@posteo.de>
2018-12-10 23:12:14 +0100

    add test fpr 2015 day6

---
+++ y2015/test/test_day06.py
@@ -0,0 +1,24 @@
+from y2015.day06 import *
+
+
+def test_part1():
+    l1, l2 = fill_lights("turn on 0,0 through 999,999")
+    assert part1(l1) == 1000000
+
+    l1, l2 = fill_lights("toggle 0,0 through 999,0")
+    assert part1(l1) == 1000
+
+    l1, l2 = fill_lights("turn off 499,499 through 500,500")
+    assert part1(l1) == 0
+
+    l1, l2 = fill_lights("""turn on 0,0 through 999,999
+toggle 0,0 through 999,0
+turn off 499,499 through 500,500""")
+    assert part1(l1) == 998996
+
+
+def test_part2():
+    l1, l2 = fill_lights("turn on 0,0 through 0,0")
+    assert part2(l2) == 1
+    l1, l2 = fill_lights("toggle 0,0 through 999,999")
+    assert part2(l2) == 2000000
