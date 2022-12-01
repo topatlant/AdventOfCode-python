4f02e0f60b56b4e3ed1c13a21e61542d0ef3d69e
Tobias Göttfert <tobias.goettfert@dlr.de>
2022-12-01 18:53:14 +0100

    add part2 of 2020-07

--- y2020/day07.py
+++ y2020/day07.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+SHINY_GOLD = "shiny gold"
+
 
 def get_input():
     with open("inputs/07.txt") as f:
@@ -9,20 +11,23 @@ def get_input():
 def main():
     puzzle = get_input()
     print("Part1:", part1(puzzle))
-    # print("Part2:", part2(puzzle))
+    print("Part2:", part2(puzzle))
 
 
 def part1(puzzle):
-    all_bags = {}
-    for line in puzzle:
-        b = Bag.from_line(line)
-        all_bags[b.color] = b
+    all_bags = parse_bags(puzzle)
 
     return sum(
         can_contain_shiny_gold(all_bags, color)
         for color in all_bags
-        if color != "shiny gold"
+        if color != SHINY_GOLD
     )
+
+
+def part2(puzzle):
+    all_bags = parse_bags(puzzle)
+    return contains_recursive(all_bags, SHINY_GOLD) - 1
+    # -1 -> without the shiny gold bag itself
 
 
 @dataclass
@@ -51,13 +56,29 @@ class Bag:
         return qty, color
 
 
-def can_contain_shiny_gold(all_bags: dict[str, Bag], bag_color: str):
-    if bag_color == "shiny gold":
+def parse_bags(puzzle: list[str]) -> dict[str, Bag]:
+    all_bags = {}
+    for line in puzzle:
+        b = Bag.from_line(line)
+        all_bags[b.color] = b
+    return all_bags
+
+
+def can_contain_shiny_gold(all_bags: dict[str, Bag], bag_color: str) -> bool:
+    if bag_color == SHINY_GOLD:
         return True
     for sub_bag in all_bags[bag_color].contains:
         if can_contain_shiny_gold(all_bags, sub_bag[1]):
             return True
     return False
+
+
+def contains_recursive(all_bags: dict[str, Bag], bag_color: str) -> int:
+    bag = all_bags[bag_color]
+    if not bag.contains:
+        return 1  # bag itself, no sub-bags
+
+    return 1 + sum(b[0] * contains_recursive(all_bags, b[1]) for b in bag.contains)
 
 
 if __name__ == "__main__":
--- y2020/test/test_day07.py
+++ y2020/test/test_day07.py
@@ -14,3 +14,17 @@ dotted black bags contain no other bags.
 
 def test_part1():
     assert part1(puzzle) == 4
+
+
+def test_part2():
+    assert part2(puzzle) == 32
+
+    puzzle2 = """shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.""".splitlines()
+
+    assert part2(puzzle2) == 126
