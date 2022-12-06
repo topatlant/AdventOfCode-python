8a5a24035716237ca15040ba0c92b52bc47dc7c0
Tobias Göttfert <tobias.goettfert@dlr.de>
2022-12-06 13:19:46 +0100

    add 2017-03

---
+++ y2017/day03.py
@@ -0,0 +1,80 @@
+def get_input():
+    return 325489
+
+
+def main():
+    puzzle = get_input()
+    print("Part 1:", part1(puzzle))
+    print("Part 2:", part2(puzzle))
+
+
+def part1(number: int) -> int:
+    # Manhattan distance
+    coords = get_coords(number)
+    return abs(coords[0]) + abs(coords[1])
+
+
+def part2(threshold: int) -> int:
+    grid = {(0, 0): 1}
+    value = 1
+    spiral_walk = spiral_iterator()
+
+    while value <= threshold:
+        coords = next(spiral_walk)
+        value = sum(grid[(x, y)] for x, y in neighbors(coords) if (x, y) in grid)
+        grid[coords] = value
+    return value
+
+
+def get_coords(n: int) -> tuple[int, int]:
+    # I did not bother to rewrite the solution of part1
+    # with the iterator from part2, because I like it so much ;)
+    directions = walk_in_spiral(n)[: n - 1]
+    x = directions.count("r") - directions.count("l")
+    y = directions.count("u") - directions.count("d")
+    return x, y
+
+
+def walk_in_spiral(n: int) -> str:
+    """get a string of directions that is at least as long (or longer than needed)
+    to take you to the n-th position"""
+    k = 1
+    res = ""
+    while len(res) < n - 1:
+        res += "r" * k  # right
+        res += "u" * k  # up
+        k += 1
+        res += "l" * k  # left
+        res += "d" * k  # down
+        k += 1
+    return res
+
+
+def spiral_iterator():
+    x, y = 0, 0
+    k = 1
+    while True:
+        for i in range(k):  # right
+            x += 1
+            yield x, y
+        for i in range(k):  # up
+            y += 1
+            yield x, y
+        k += 1
+        for i in range(k):  # left
+            x -= 1
+            yield x, y
+        for i in range(k):  # down
+            y -= 1
+            yield x, y
+        k += 1
+
+
+def neighbors(coord):
+    for i in (-1, 0, 1):
+        for j in (-1, 0, 1):
+            yield coord[0] + i, coord[1] + j
+
+
+if __name__ == "__main__":
+    main()
---
+++ y2017/test/test_day03.py
@@ -0,0 +1,12 @@
+from y2017.day03 import *
+
+
+def test_part1():
+    assert part1(1) == 0
+    assert part1(12) == 3
+    assert part1(23) == 2
+    assert part1(1024) == 31
+
+
+def test_part2():
+    assert part2(800) == 806
