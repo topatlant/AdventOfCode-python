fba8434e1788810f5cb643bfc562ed840f5f289d
Tobias Göttfert <tobias.goettfert@dlr.de>
2022-12-07 17:37:52 +0100

    add 2022-07

---
+++ y2022/day07.py
@@ -0,0 +1,115 @@
+from pathlib import PurePath
+from typing import Iterator
+
+
+def get_input():
+    with open("inputs/07.txt") as f:
+        return f.read().splitlines()
+
+
+def main():
+    puzzle = get_input()
+    print("Part 1:", part1(puzzle))
+    print("Part 2:", part2(puzzle))
+
+
+def part1(puzzle: list[str]) -> int:
+    all_dirs = populate_dirs(puzzle)
+    return sum(
+        x for x in (d.get_total_size() for d in all_dirs.values()) if x <= 100000
+    )
+
+
+def part2(puzzle: list[str]) -> int:
+    all_dirs = populate_dirs(puzzle)
+    total_cap = int(70e6)
+    space_needed = int(30e6)
+    total_used = all_dirs[str(PurePath("/"))].get_total_size()
+
+    space_free = total_cap - total_used
+    space_still_needed = space_needed - space_free
+
+    return min(
+        x
+        for x in (d.get_total_size() for d in all_dirs.values())
+        if x >= space_still_needed
+    )
+
+
+class File:
+    def __init__(self, name: str, size: int):
+        self.name = name
+        self.size = size
+
+    @staticmethod
+    def from_line(line: str) -> "File":
+        size, name = line.split()
+        return File(name, int(size))
+
+
+class Directory:
+    def __init__(self, name: str):
+        self.name = name
+        self.files = []
+        self.subdirs = []
+
+    @staticmethod
+    def from_ls_output(name: str, ls: list[str]) -> "Directory":
+        files = []
+        subdirs = []
+        for line in ls:
+            if line.startswith("dir"):
+                subdirs.append(line.split()[1])
+            else:
+                files.append(File.from_line(line))
+        d = Directory(name)
+        d.files = files
+        d.subdirs = subdirs
+        return d
+
+    def add_subdir(self, sd: "Directory"):
+        """replace string placeholder with Directory object"""
+        if sd.name in self.subdirs:
+            self.subdirs.remove(sd.name)
+        self.subdirs.append(sd)
+
+    def get_total_size(self) -> int:
+        return sum(f.size for f in self.files) + sum(
+            d.get_total_size() for d in self.subdirs
+        )
+
+
+def populate_dirs(puzzle: list[str]) -> dict[str, Directory]:
+    all_dirs = {}
+    cur_dir = PurePath("/")
+    commands = split_by_dollars(puzzle)
+    # assert commands[0]=="$ cd /"
+    for command, output in commands:
+        if command.startswith("$ cd"):
+            if command == "$ cd ..":
+                cur_dir = cur_dir.parent
+            else:
+                cur_dir = cur_dir / (command.split()[-1])
+        elif command.startswith("$ ls"):
+            d = Directory.from_ls_output(cur_dir.name, output)
+            all_dirs[str(cur_dir)] = d
+            if cur_dir != PurePath("/"):
+                # don't add root path to itself
+                all_dirs[str(cur_dir.parent)].add_subdir(d)
+        else:
+            raise ValueError("Invalid terminal command")
+    return all_dirs
+
+
+def split_by_dollars(lines: list[str]) -> Iterator[tuple[str, list[str]]]:
+    acc = [lines[0]]
+    for line in lines[1:]:
+        if line.startswith("$"):
+            yield acc[0], acc[1:]
+            acc = []
+        acc.append(line)
+    yield acc[0], acc[1:]
+
+
+if __name__ == "__main__":
+    main()
---
+++ y2022/test/test_day07.py
@@ -0,0 +1,29 @@
+from y2022.day07 import *
+
+puzzle = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k""".splitlines()
+
+
+def test_part1():
+    assert part1(puzzle) == 95437
