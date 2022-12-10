fd6af54ab0ef100a0a15261680d3da98d28c7520
Tobias Göttfert <tobias.goettfert@dlr.de>
2022-12-10 22:48:35 +0100

    add test for 2022-10 part2 and requirements.txt

--- y2022/day10.py
+++ y2022/day10.py
@@ -1,6 +1,3 @@
-from common import split_by_blanks
-
-
 def get_input():
     with open("inputs/10.txt") as f:
         return f.readlines()
@@ -28,39 +25,43 @@ def part2(puzzle: list[str]) -> str:
     return crt
 
 
-def execute_instructions(puzzle) -> list[int]:
+def execute_instructions(puzzle: list[str]) -> list[int]:
     values_during = []
-    X = 1
+    x = 1
 
     for instruction in puzzle:
 
         if instruction.startswith("noop"):
-            values_during.append(X)
+            values_during.append(x)
 
         else:
             assert instruction.startswith("addx")
-            values_during.append(X)
-            values_during.append(X)
+            values_during.append(x)
+            values_during.append(x)
             delta = int(instruction.split()[1])
-            X += delta
+            x += delta
 
-    values_during.append(X)
+    values_during.append(x)
     return values_during
 
 
-def extract_interesting_signal_strenghts(values_during) -> int:
-    return sum(cycle * value_X_during_cycle(cycle, values_during) for cycle in range(20, 221, 40))
+def extract_interesting_signal_strenghts(values_during: list[int]) -> int:
+    return sum(
+        cycle * value_x_during_cycle(cycle, values_during)
+        for cycle in range(20, 221, 40)
+    )
 
 
-def value_X_during_cycle(cycle, values_during) -> int:
+def value_x_during_cycle(cycle: int, values_during: list[int]) -> int:
     # index 0 = X during cycle 1
     # index 1 = X after cycle 1 and during cycle 2
+    # ...
     return values_during[cycle - 1]
 
 
-def get_crt_pixel(cycle, values_during) -> str:
+def get_crt_pixel(cycle: int, values_during: list[int]) -> str:
     x_beam = (cycle - 1) % 40
-    x_sprite = value_X_during_cycle(cycle, values_during)
+    x_sprite = value_x_during_cycle(cycle, values_during)
     return "#" if abs(x_beam - x_sprite) <= 1 else "."
 
 
