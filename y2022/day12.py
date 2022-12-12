1c443bd097e963a7e41aa8a436068f050ad6c9b9
Tobias Göttfert <tobias.goettfert@dlr.de>
2022-12-12 22:17:21 +0100

    harmonize part1&2 of 2022-12
    and add ascii visualisation of pathes
    that are found

--- y2022/day12.py
+++ y2022/day12.py
@@ -1,13 +1,15 @@
+import os
 import numpy as np
-from typing import Union, Iterator, Optional
+from typing import Union, Iterator
 
-MIN_SO_FAR: int  # just for printing
+MIN_SO_FAR: int  # I have to find a better way than this global variable
+MAX_DEPTH = 800  # tune this so that maximum recursion depth is not exceeded - final solution will be below 800 ;)
 
 
-# Yes, I know this is not optimal - I thought about doing the full A*
+# Yes, I know this solution is not optimal - I thought about e.g. doing the full A*
 # but this is also quick enough to do the job ;)
-# does depth-first recursion with manhattan-distance to target as the priority metrics
-# and remembers visited points with shortest known path until then
+# Does depth-first recursion with manhattan-distance to target as the priority metrics
+# and remembers visited points with lowest step count until then.
 
 
 def get_input():
@@ -18,49 +20,36 @@ def get_input():
 def main():
     puzzle = get_input()
     print("Part 1:", part1(puzzle))
-    puzzle = (
-        get_input()
-    )  # we have to get the input again, since my clever algorithm is destroying it :P
+    # we have to get the input again, since my clever setup algorithm is destroying it :P
+    puzzle = get_input()
     print("Part 2:", part2(puzzle))
 
 
-def part1(puzzle) -> int:
+def part1(puzzle, pt2=False) -> int:
+    # Since only the number of steps is needed, I am turning start and target around.
+    # This makes it easier for part2
+    # We will therefore allow a max *descending* of one per step, instead of ascending...
+
     global MIN_SO_FAR
     MIN_SO_FAR = 10000
 
     heights, start, target = parse(puzzle)
-
+    start, target = target, start  # search from target to start
     path = np.zeros_like(heights, "i")
     already_seen = {}
 
-    min_steps = step(heights, path, already_seen, current=start, target=target)
+    min_steps = step(heights, path, already_seen, current=start, target=target, pt2=pt2)
 
     print("done!")
     return min_steps
 
 
 def part2(puzzle) -> int:
-    global MIN_SO_FAR
-    MIN_SO_FAR = 10000
-
-    # ok, let's turn this around
-    # find the shortest path from E to any square of height a
-    # but only descend max one step at a time
-    heights, start, target = parse(puzzle)
-    start, target = target, start  # hehe
-
-    path = np.zeros_like(heights, "i")
-    already_seen = {}
-
-    min_steps = step(
-        heights, path, already_seen, current=start, target=target, pt2=True
-    )
-
-    print("done!")
-    return min_steps
+    return part1(puzzle, pt2=True)
 
 
 def parse(puzzle: list[str]) -> tuple[np.array, tuple[int, int], tuple[int, int]]:
+    """parse the puzzle destructively, i.e. replace start with "a" and target with "z" """
     def get_height(char: str):
         return ord(char) - 96
 
@@ -97,18 +86,18 @@ def step(
     already_seen: dict[tuple[int, int], int],
     current: tuple[int, int],
     target: tuple[int, int],
-    pt2=False,
+    pt2: bool,
 ) -> Union[int, bool]:
     global MIN_SO_FAR
 
-    n_steps = number_of_steps(path)
-
     # remember the path that brought us here
-    path[current] = path.max() + 1
+    n_steps = path.max()
+    path[current] = n_steps + 1
 
     # did we finish?
     if current == target or (pt2 and heights[current] == 1):
         if n_steps < MIN_SO_FAR:
+            # os.system("cls")  # for Windows systems
             print(f"Found a path with {n_steps} steps!")
             # print(visualize_path(path))
             MIN_SO_FAR = n_steps
@@ -117,29 +106,32 @@ def step(
     # stop if we already know a better path to here - or else update knowledge.
     # Additionally stop if we are about to exceed the default recursion depth of Python ;)
     # don't worry - there exist solutions smaller than this
-    if n_steps > 800 or (current in already_seen and already_seen[current] <= n_steps):
+    if (
+        n_steps > MAX_DEPTH
+        or n_steps > MIN_SO_FAR
+        or (current in already_seen and already_seen[current] <= n_steps)
+    ):
         return False
-    else:
-        already_seen[current] = n_steps
 
-    # check all options from here
-    n_steps = []
+    already_seen[current] = n_steps
+
+    # check all options from here, starting with steps that bring us closer to target
+    n_steps_children = []
     next_to_check = sorted(
-        allowed_positions(heights, path, current, pt2),
+        allowed_positions(heights, path, current),
         key=lambda p: manhattan_distance(p, target),
     )
     for next_position in next_to_check:
-        # check directions closer to target first
         # we have to branch (copy the visited path) to be able to do recursion/backtracking/different pathes
-        n_steps.append(
+        n_steps_children.append(
             step(heights, path.copy(), already_seen, next_position, target, pt2)
         )
 
-    if n_steps == [] or all(x is False for x in n_steps):
+    if not n_steps_children or all(x is False for x in n_steps_children):
         # no route to target from here
         return False
 
-    return min(n for n in n_steps if n is not False)
+    return min(n for n in n_steps_children if n is not False)
 
 
 def manhattan_distance(p1: tuple[int, int], p2: tuple[int, int]) -> int:
@@ -147,36 +139,38 @@ def manhattan_distance(p1: tuple[int, int], p2: tuple[int, int]) -> int:
 
 
 def allowed_positions(
-    heights: np.array, visited: np.array, current: tuple[int, int], pt2: bool
+    heights: np.array, visited: np.array, current: tuple[int, int]
 ) -> Iterator[tuple[int, int]]:
     def check_height(p):
-        # cannot climb more than one height per step
-        # or in pt2 descend more than one, since we reversed the search
-        if pt2:
-            return heights[p] >= heights[current] - 1
-        else:
-            return heights[p] <= heights[current] + 1
+        # cannot *descend* more than one height per step since we are searching in opposite direction
+        return heights[p] >= heights[current] - 1
 
     def check_circular(p):
         # do not visit the same spot twice
         return not visited[p]
 
     def check_boundary(p):
+        # do not leave grid
         return 0 <= p[0] < heights.shape[0] and 0 <= p[1] < heights.shape[1]
 
     for delta in (-1, 1):
-        # horizontal step
+        # vertical step
         new = current[0] + delta, current[1]
         if check_boundary(new) and check_circular(new) and check_height(new):
             yield new
-        # vertical step
+        # horizontal step
         new = current[0], current[1] + delta
         if check_boundary(new) and check_circular(new) and check_height(new):
             yield new
 
 
-def number_of_steps(visited: np.array) -> int:
-    return visited.max()
+def visualize_path(path: np.array):
+    res = ""
+    for i in range(path.shape[0]):
+        for j in range(path.shape[1]):
+            res += "#" if path[i, j] else " "
+        res += "\n"
+    return res
 
 
 if __name__ == "__main__":
